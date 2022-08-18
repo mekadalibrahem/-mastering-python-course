8ea1657b2245c488c34d2f5ecf86ab13e297d3c1
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-08-18 23:20:21 +0300

    add code week 05

---
+++ codes/week05/035 - Assignment Operators.py
@@ -0,0 +1,4 @@
+""" 
+035 - Assignment Operators 
+
+"""
---
+++ codes/week05/038 - User Input.py
@@ -0,0 +1,17 @@
+""" 
+link : https://www.youtube.com/watch?v=2EY1CCnByK4&list=PLDoPjvoNmBAyE_gei5d18qkfIe-Z8mocs
+"""
+
+# ----------------
+# -- User Input --
+# ----------------
+
+fName = input('What\'s Is Your First Name?')
+mName = input('What\'s Is Your Middle Name?')
+lName = input('What\'s Is Your Last Name?')
+
+fName = fName.strip().capitalize()
+mName = mName.strip().capitalize()
+lName = lName.strip().capitalize()
+
+print(f"Hello {fName} {mName:.1s} {lName} Happy To See You.")
