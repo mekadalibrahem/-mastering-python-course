ed25dcb1ebc3d57b917543e8291e5ab6e27cb434
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-08-18 23:19:46 +0300

    add code week 01

---
+++ codes/week01/003 - Syntax And Your First App .py
@@ -0,0 +1,14 @@
+""" 
+
+
+"""
+
+print("I Love Python")
+print("I Love Programming")
+
+""" 
+
+link :https://www.youtube.com/watch?v=xiMHoMVWdI4&list=PLDoPjvoNmBAyE_gei5d18qkfIe-Z8mocs
+
+
+"""
---
+++ codes/week01/007 - Variables Part One .py
@@ -0,0 +1,35 @@
+# --------------------------------------
+# -- Variables --
+# ---------------
+# Syntax => [Variable Name] [Assignment Operator] [Value]
+#
+# Name Convention and Rules
+# [1] Can Start With (a-z A-Z) Or Underscore
+# [2] You Cannot With Num Or Special Characters
+# [3] Can Include (0-9) Or Underscore
+# [4] Cannot Include Special Characters
+# [5] Name is Not Like name [ Case Sensitive ]
+# --------------------------------------
+
+name = "Osama Elzero"  # Single Word => Normal
+myName = "Osama Elzero"  # Two Words => camelCase
+my_name = "Osama Elzero"  # Two Words => snake_case
+
+print(name)
+print(myName)
+print(my_name)
+
+""" 
+ملاحظات الدرس
+بعض المعلومات التي تتعلمها عن المتغيرات يمكن أن تختلف من لغة للغة أخرى وبتعلمك اللغات سوف تعرف الفروق بنفسك.
+المراجع
+
+    Real Python
+    Guru 99
+    Learn Python
+    W3Schools
+
+
+link : https://www.youtube.com/watch?v=hQnZxqp3Q0Y&list=PLDoPjvoNmBAyE_gei5d18qkfIe-Z8mocs
+
+"""
