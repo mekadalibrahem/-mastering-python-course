09df0ac8f6f41d1b55941f2d40bf5ce98fb1f5a5
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-08-18 23:25:03 +0300

    Assignments Week 01 Assignment 04

---
+++ Assignments/Week01.L001_L010/Assignment04.py
@@ -0,0 +1,10 @@
+# التكليف 04
+# قم بعمل Concatenate للمتغيرات مع بعض الكلمات لتخرج بهذه الرسالة الموجودة في ال Output التالي
+# "Hello, My Name Is Osama And Iam 38 Years Old and I Live in Egypt."
+# =========================================================
+Name = "Mekad"
+Age = "21" 
+Country = "Syria";
+
+print("Hello, My   Name Is "+Name+ " And Iam "+Age+" Years Old and I Live in" + Country+ ".") ;
+
