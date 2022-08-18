a891d9172784b2ed582d30d90642e6c3953bbc5f
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-08-18 23:27:29 +0300

    Assignments Week 05 part 01

---
+++ Assignments/Week05.L033_L037/Assignment01.py
@@ -0,0 +1,29 @@
+# التكليف 01
+
+#     في أول 4 أسطر وبإستخدام Bool Method قم بطباعة 4 أنواع بيانات مختلفة نتيجتهم هي True
+#     في السطر الخامس إلى الثامن وبإستخدام Bool Method قم بطباعة 4 أنواع بيانات مختلفة نتيجتهم هي False
+
+# # Needed Output
+
+# True
+# True
+# True
+# True
+# False
+# False
+# False
+# False
+# ===============================
+
+#  True
+print(bool(1))
+print(bool(0.8))
+print(bool("a"))
+print(bool(["s"]))
+print(bool(True))
+# False
+print(bool(0))
+print(bool(""))
+print(bool([]))
+print(bool(0.0))
+print(bool(False))
