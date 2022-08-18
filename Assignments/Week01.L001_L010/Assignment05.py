82585faa8121c0ff5788c7d13ebd32742afe80a3
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-08-18 23:25:18 +0300

    Assignments Week 01 Assignment 05

---
+++ Assignments/Week01.L001_L010/Assignment05.py
@@ -0,0 +1,11 @@
+# التكليف 05
+# قم بطباعة نوع كل متغير من المتغيرات التي أنشأناها في سطر منفصل ليظهر لنا ال Output التالي 
+
+
+Name = "Mekad"
+Age = "21" 
+Country = "Syria";
+
+print(type(Name));
+print(type(Age));
+print(type(Country));
