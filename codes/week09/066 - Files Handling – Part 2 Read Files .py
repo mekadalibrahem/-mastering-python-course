0326ce4430fcc50bbf10a3589d7f935e98eb2de0
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-08-19 21:37:06 +0300

    add code  week 09

---
+++ codes/week09/066 - Files Handling – Part 2 Read Files .py
@@ -0,0 +1,37 @@
+''' 
+066 - Files Handling – Part 2 Read Files  
+link : https://www.youtube.com/watch?v=9ZU9FQQbOYE&list=PLDoPjvoNmBAyE_gei5d18qkfIe-Z8mocs
+''' 
+# --------------------------------
+# -- File Handling => Read File --
+# --------------------------------
+
+myFile = open("D:\Python\Files\osama.txt", "r")
+
+print(myFile)  # File Data Object
+print(myFile.name)
+print(myFile.mode)
+print(myFile.encoding)
+
+print(myFile.read())
+print(myFile.read(5))
+
+print(myFile.readline(5))
+print(myFile.readline())
+print(myFile.readline())
+
+print(myFile.readlines())
+print(myFile.readlines(50))
+print(type(myFile.readlines()))
+
+for line in myFile:
+
+  print(line)
+
+  if line.startswith("07"):
+
+    break
+
+# Close The File
+
+myFile.close()
---
+++ codes/week09/067 - Files Handling – Part 3 Write and Append in Files .py
@@ -0,0 +1,22 @@
+''' 
+067 - Files Handling – Part 3 Write and Append in Files  
+link : https://www.youtube.com/watch?v=FBcElrNaiZQ&list=PLDoPjvoNmBAyE_gei5d18qkfIe-Z8mocs
+''' 
+# -----------------------------------------------
+# -- File Handling => Write and Append In File --
+# -----------------------------------------------
+
+myFile = open("D:\Python\Files\osama.txt", "w")
+myFile.write("Hello\n")
+myFile.write("Third Line")
+
+myFile = open(r"D:\Python\Files\fun.txt", "w")
+myFile.write("Elzero Web School\n" * 1000)
+
+myList = ["Oasma\n", "Ahmed\n", "Sayed\n"]
+
+myFile = open("D:\Python\Files\osama.txt", "w")
+myFile.writelines(myList)
+
+myFile = open("D:\Python\Files\osama.txt", "a")
+myFile.write("Elzero")
