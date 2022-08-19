f7cf348565d11ccd157f1c342388eef804890e2d
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-08-19 22:07:08 +0300

    addd code week 12

---
+++ codes/week12/089 - Installing And Use Pylint For Better Code .py
@@ -0,0 +1,20 @@
+''' 
+089 - Installing And Use Pylint For Better Code 
+link : https://www.youtube.com/watch?v=YvqKqam_3zY&list=PLDoPjvoNmBAyE_gei5d18qkfIe-Z8mocs
+'''
+# -----------------------------------------------
+# -- Installing And Use Pylint For Better Code --
+# -----------------------------------------------
+
+"""
+This is My Module
+To Create Function
+To Say Hello
+"""
+
+def say_hello(name):
+  '''This Function Only Say Hello To Someone'''
+  msg = "Hello"
+  return f"{msg} {name}"
+
+say_hello("Ahmed")
---
+++ codes/week12/094 - Type Hinting .py
@@ -0,0 +1,19 @@
+''' 
+094 - Type Hinting 
+link : https://www.youtube.com/watch?v=J_e-r7NcwPU&list=PLDoPjvoNmBAyE_gei5d18qkfIe-Z8mocs
+'''
+# ------------------
+# -- Type Hinting --
+# ------------------
+
+def say_hello(name) -> str:
+
+  print(f"Hello {name}")
+
+say_hello("Ahmed")
+
+def calculate(n1, n2) -> str:
+
+  print(n1 + n2)
+
+calculate(10, 40)
