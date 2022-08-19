55b2bb0932781c371404d53dbd0d712f4facb1ac
Mekad Alibrahem <mekadalibrahem123@gmail.com>
2022-08-19 19:55:03 +0300

    add codes Week 07

---
+++ codes/week07/047 - Loop – While and Else.py
@@ -0,0 +1,24 @@
+""" 
+ 047 - Loop – While and Else 
+ link  : https://www.youtube.com/watch?v=A0oBGPSUbeI&list=PLDoPjvoNmBAyE_gei5d18qkfIe-Z8mocs
+"""
+# -------------------
+# -- Loop => While --
+# -------------------
+# while condition_is_true
+#   Code Will Run Until Condition Become False
+# -----------------------
+
+a = 0
+
+while a < 15:
+
+  print(a)
+
+  a += 1  # a = a + 1
+
+print("Loop is Done")  # True Become False
+
+while False:
+
+  print("Will Not Print")
---
+++ codes/week07/053 - Loop – For Nested Loops.py
@@ -0,0 +1,56 @@
+""" 
+053 - Loop – For Nested Loops  
+link : https://www.youtube.com/watch?v=x_GyjV2Nb6k&list=PLDoPjvoNmBAyE_gei5d18qkfIe-Z8mocs
+""" 
+# -----------------
+# -- Loop => For --
+# -- Nested Loop --
+# -----------------
+
+peoples = ["Osama", "Ahmed", "Sayed", "Ali"]
+
+skills = ['Html', 'Css', 'Js']
+
+for name in peoples:  # Outer Loop
+
+  print(f"{name} Skills Is: ")
+
+  for skill in skills:  # Inner Loop
+
+    print(f"- {skill}")
+
+# Dictionary
+
+peoples = {
+  "Osama": {
+    "Html": "70%",
+    "Css": "80%",
+    "Js": "70%"
+  },
+  "Ahmed": {
+    "Html": "90%",
+    "Css": "80%",
+    "Js": "90%"
+  },
+  "Sayed": {
+    "Html": "70%",
+    "Css": "60%",
+    "Js": "90%"
+  }
+}
+
+print(peoples["Osama"])
+print(peoples["Ahmed"])
+print(peoples["Sayed"])
+
+print(peoples["Osama"]['Css'])
+print(peoples["Ahmed"]['Css'])
+print(peoples["Sayed"]['Css'])
+
+for name in peoples:
+
+  print(f"Skills and Progress For {name} Is: ")
+
+  for skill in peoples[name]:
+
+    print(f"{skill.upper()} => {peoples[name][skill]}")
